f5c07344066271282a28f8c9cb9d78c5295b5233
Obi <F-frozen-93@mail.ru>
2023-02-27 21:48:51 +0400

    test

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.center = "";
+        this.left = "";
+        this.right = "";
+    }
+
 
     setRange(min, max) {
-
+this.min = min;
+this.max = max
     }
 
     guess() {
-
+return this.center = (this.left+this.right)/2;
     }
 
     lower() {
-
+    return this.right = this.center
     }
 
     greater() {
-
+        return this.left = this.center
     }
 }
 
